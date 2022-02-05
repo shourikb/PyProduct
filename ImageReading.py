a1647d3a064c156b78a22be7b86972c5d5277329
Shourik <shourik2005@gmail.com>
2022-02-05 12:48:58 -0800

    basic image reading

--- ImageReading.py
+++ ImageReading.py
@@ -1,2 +1,14 @@
 import pytesseract
 import cv2
+
+breaking_news = cv2.imread('images/breakingnews.png')
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Shourik\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+text = pytesseract.image_to_string(breaking_news)
+
+bitcoin = cv2.imread('images/bitcoin.jpeg')
+
+text = pytesseract.image_to_string(bitcoin)
+
+print(text)
